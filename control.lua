2e406b5fd2f8fc145826563d27650659205228a3
Dmitriy Dergachev <dergachevdv90@gmail.com>
2018-11-13 17:43:24 +0300

    Update control.lua

--- control.lua
+++ control.lua
@@ -2,14 +2,16 @@ local surfaceName = "metrorio"
 
 local function init()
     if global.surface == nil then
-        local isset = false
-        for _, surface in pairs(game.surfaces) do
-            if (surface.name == surfaceName) then
-                isset = true
+        local surface = game.surfaces[surfaceName]
+        if surface == nil then
+            if #(game.sufaces) < 256 then
+                global.surface = game.create_surface(surfaceName, {width = 2, height = 2})
+                global.daytime = 0
+                gloabl.freez_daytime = true
+            else
+                error("Unfortunately you have no available surfaces left for Metrorio. You cannot use Metrorio on this map")
             end
         end
-        if isset == false then
-            global.surface = game.create_surface(surfaceName, {width = 2, height = 2})
-        end
+        global.surface = surface
     end
 end
