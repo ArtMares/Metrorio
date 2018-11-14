1852a4c6b0be54169b95ad2039517ed7cb305011
Dmitriy Dergachev <dergachevdv90@gmail.com>
2018-11-14 17:31:14 +0300

    Add Class Metrorio

--- control.lua
+++ control.lua
@@ -1,47 +1,17 @@
 local inspect = require("inspect")
 
-local surfaceName = "metrorio"
+require("metro.metrorio")
 
-local function copyChanks()
-    for chunk in game.surfaces[1].get_chunks() do
-        log("x: " .. chunk.x .. ", y: " .. chunk.y)
-    end
-end
-
-local function init()
-    if global.surface == nil then
-        local surface = game.surfaces[surfaceName]
-        if surface == nil then
-            if #(game.surfaces) < 256 then
-                global.surface = game.create_surface(surfaceName, {width = 2, height = 2})
-                global.daytime = 0
-                global.freez_daytime = true
-            else
-                error("Unfortunately you have no available surfaces left for Metrorio. You cannot use Metrorio on this map")
-            end
-        end
-        global.surface = surface
-    end
-end
+local metro = Metrorio("underground")
 
 script.on_init(function()
-    init()
-    copyChanks()
+    metro.initSurface()
+end)
+
+script.on_configuration_changed(function(data)
+    metro.initSurface()
 end)
 
 script.on_event(defines.events.on_player_joined_game, function(event)
---    init()
-    copyChanks()
-    for _, entity in pairs(game.entity_prototypes) do
-        if entity.type == "lab" then
-            log(inspect.inspect(entity))
-        end
-    end
-end)
 
---script.on_event(defines.events.on_chunk_generated, function(event)
---    local box = event.area
---    local surface = event.surface
---    game.print(box.left_top.x .. " " .. box.left_top.y .. " -> " .. box.right_bottom.x .. " " .. box.right_bottom.y)
---    game.print(surface.name)
---end)
+end)
---
+++ prototypes/tile.lua
@@ -0,0 +1,72 @@
+
+local function makeTile(tinfo)
+    data:extend(
+        {
+            type = "tile",
+            name = tinfo.name,
+            needs_correction = false,
+            collision_mask = tinfo.collision_mask,
+            layer = tinfo.layer or 50,
+            variant = {
+                main = tinfo.pictures,
+                inner_corner =  {picture = M .. "/graphics/nothing.png", count = 0},
+                outer_corner =  {picture = M .. "/graphics/nothing.png", count = 0},
+                side =          {picture = M .. "/graphics/nothing.png", count = 0},
+                u_transition =  {picture = M .. "/graphics/nothing.png", count = 0},
+                o_transition =  {picture = M .. "/graphics/nothing.png", count = 0}
+            },
+            walking_speed_modifier = 1.0,
+            walking_sound = {
+                {
+                    filename = "__base__/sound/walking/concrete-01.ogg",
+                    volume = 1.2
+                },
+                {
+                    filename = "__base__/sound/walking/concrete-02.ogg",
+                    volume = 1.2
+                },
+                {
+                    filename = "__base__/sound/walking/concrete-03.ogg",
+                    volume = 1.2
+                },
+                {
+                    filename = "__base__/sound/walking/concrete-04.ogg",
+                    volume = 1.2
+                }
+            },
+            map_color = tinfo.map_color or {r = 1},
+            ageing = 0.0006
+        }
+    )
+end
+
+local function wallMask()
+    return {
+        "groud-tile",
+        "water-tile",
+        "resource-layer",
+        "floor-layer",
+        "item-layer",
+        "object-layer",
+        "player-layer",
+        "doodad-layer"
+    }
+end
+
+local function edgeMask()
+    return {
+        "groud-tile",
+        "water-tile",
+        "resource-layer",
+        "floor-layer",
+        "item-layer",
+        "object-layer",
+        "doodad-layer"
+    }
+end
+
+local function floorMask()
+    return {
+        "ground-tile"
+    }
+end
